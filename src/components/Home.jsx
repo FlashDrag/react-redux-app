1a4bcbda672a3fa7d3bc1680d3269c64f1e74781
flashdrag <flashdrag@gmail.com>
2023-12-01 14:49:52 +0000

    Add cart functionality to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
 import productList from "../data/productList.json";
+import cartSlice from "../store/cart/cartSlice";
 import "../styles/home.scss";
 
 const Home = () => {
+  const { cartProductIds } = useSelector((state) => state.cart);
+  const { addToCart, removeFromCart } = cartSlice.actions;
+  const dispatch = useDispatch();
+
   return (
     <div className="container product-catalogue">
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
@@ -20,7 +26,22 @@ const Home = () => {
                 <div className="card-body text-center flex-grow-0">
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text ">{product.price}</p>
-                  <button className="btn btn-primary">Add to cart</button>
+                  {!cartProductIds.includes(product.id) && (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => dispatch(addToCart(product.id))}
+                    >
+                      Add to cart
+                    </button>
+                  )}
+                  {cartProductIds.includes(product.id) && (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => dispatch(removeFromCart(product.id))}
+                    >
+                      Remove from cart
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
