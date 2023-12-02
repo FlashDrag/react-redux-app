2990180025d6e60e5e6ae00c0487e5b3dbccdfee
flashdrag <flashdrag@gmail.com>
2023-12-02 13:51:01 +0000

    Refactor Home component to handle loading and error states

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,44 +15,50 @@ const Home = () => {
 
   return (
     <div className="container product-catalogue">
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
-        {products.data.map((product) => {
-          return (
-            <div className="wrapper col mb-4" key={product.id}>
-              <div className="card h-100 d-flex align-items-center">
-                <div className="d-flex flex-grow-1 flex-wrap align-items-center">
-                  <img
-                    src={product.imageUrl}
-                    alt={product.name}
-                    className="card-img-top p-3"
-                  />
-                </div>
+      {products.fetchStatus === "loading" ? (
+        <h1>Loading...</h1>
+      ) : products.fetchStatus === "error" ? (
+        <h1>Error</h1>
+      ) : (
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+          {products.data.map((product) => {
+            return (
+              <div className="wrapper col mb-4" key={product.id}>
+                <div className="card h-100 d-flex align-items-center">
+                  <div className="d-flex flex-grow-1 flex-wrap align-items-center">
+                    <img
+                      src={product.imageUrl}
+                      alt={product.name}
+                      className="card-img-top p-3"
+                    />
+                  </div>
 
-                <div className="card-body text-center flex-grow-0">
-                  <h5 className="card-title">{product.name}</h5>
-                  <p className="card-text ">{product.price}</p>
-                  {!cart.cartProductIds.includes(product.id) && (
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => dispatch(addToCart(product.id))}
-                    >
-                      Add to cart
-                    </button>
-                  )}
-                  {cart.cartProductIds.includes(product.id) && (
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => dispatch(removeFromCart(product.id))}
-                    >
-                      Remove from cart
-                    </button>
-                  )}
+                  <div className="card-body text-center flex-grow-0">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text ">{product.price}</p>
+                    {!cart.cartProductIds.includes(product.id) && (
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => dispatch(addToCart(product.id))}
+                      >
+                        Add to cart
+                      </button>
+                    )}
+                    {cart.cartProductIds.includes(product.id) && (
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => dispatch(removeFromCart(product.id))}
+                      >
+                        Remove from cart
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
